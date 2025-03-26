c8438693961cd189f243b1f0e340be97ec65c757
xezon <4720891+xezon@users.noreply.github.com>
2025-03-26 22:22:34 +0100

    Fix shift mask in _lrotl

--- Dependencies/Utility/Utility/intrin_compat.h
+++ Dependencies/Utility/Utility/intrin_compat.h
@@ -10,6 +10,7 @@ static inline uint32_t _lrotl(uint32_t value, int shift)
 #if defined(__has_builtin) && __has_builtin(__builtin_rotateleft32)
     return __builtin_rotateleft32(value, shift);
 #else
+    shift &= 31;
     return ((value << shift) | (value >> (32 - shift)));
 #endif
 }
