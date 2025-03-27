f6c7077fb574c4fe9efbdce13cee4af969116622
Valerii Myronov <valeriy@hey.com>
2025-03-27 22:38:27 +0000

    [ZH] Always render game full screen to support ControlBar Pro

--- GeneralsMD/Code/GameEngine/Source/GameClient/GUI/GUICallbacks/ControlBarCallback.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/GUI/GUICallbacks/ControlBarCallback.cpp
@@ -511,7 +511,7 @@ void ShowControlBar( Bool immediate )
 		if (window)
 		{	
 			TheControlBar->switchControlBarStage(CONTROL_BAR_STAGE_DEFAULT);
-			TheTacticalView->setHeight((Int)(TheDisplay->getHeight() * 0.80f));
+			TheTacticalView->setHeight((Int)(TheDisplay->getHeight()));
 			if (TheControlBar->m_animateWindowManager && !immediate)
 			{
 				TheControlBar->m_animateWindowManager->reset();
@@ -592,7 +592,7 @@ void ToggleControlBar( Bool immediate )
 					TheControlBar->showSpecialPowerShortcut();
 
 				//now hidden, we're making it visible again so shrink viewport under the window
-				TheTacticalView->setHeight((Int)(TheDisplay->getHeight() * 0.80f)); 
+				TheTacticalView->setHeight((Int)(TheDisplay->getHeight()));
 				window->winHide(!window->winIsHidden());
 				TheControlBar->switchControlBarStage(CONTROL_BAR_STAGE_DEFAULT);
 				if (TheControlBar->m_animateWindowManager && !immediate)
